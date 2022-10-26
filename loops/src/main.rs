e77b9e1d5e42d3daa3ba0c3a2b576076b1734cdf
Mihail Rain Heart <mihail.rain.heart@gmail.com>
2022-10-26 23:35:05 +0300

    inverse loop
    Signed-off-by: Mihail Rain Heart <mihail.rain.heart@gmail.com>

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,23 +1,7 @@
 static N: i32 = 3; // Число повторений
 
 fn main() {
-    let mut i = 0;
-    loop {
-        println!("{}", i);
-        if i == N {
-            break;
-        }
-        i += 1;
-    }
-
-    i = 0;
-
-    while i <= N {
-        println!("{}", i);
-        i += 1
-    }
-
-    for i in 0..=N {
-        println!("{}", i)
+    for i in (0..N).rev() {
+        println!("{i}")
     }
 }
